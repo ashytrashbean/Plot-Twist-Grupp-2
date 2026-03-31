09be76c4ad18729152cd7ecec47b5166f4836810
ashytrashbean <annalu.jansson@gmail.com>
2026-03-31 14:05:15 +0200

    Testing map integration with Leaflet

---
+++ map.js
@@ -0,0 +1,6 @@
+let map = L.map('map').setView([59.61605838637805, 16.555387531026348], 12);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
