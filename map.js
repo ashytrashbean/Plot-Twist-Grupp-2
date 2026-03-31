728d3f079ec3649d807d7c86599b1019f4ca1e1b
ashytrashbean <annalu.jansson@gmail.com>
2026-03-31 16:07:25 +0200

    Added map integration with search function and to see in the console the current view (lat, long)

--- map.js
+++ map.js
@@ -1,6 +1,17 @@
-let map = L.map('map').setView([59.61605838637805, 16.555387531026348], 12);
+// inside the setView([latitude, longitude], map view distance)
+const map =L.map('map').setView([61.52, 12.74], 4);
 
+// This is purely the map object itself
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+// This function runs every single time the map stops moving to see latitude and longitude
+map.on('moveend', function() {
+    var center = map.getCenter();
+    console.log(`Map stopped at: lat: ${center.lat.toFixed(2)}, long: ${center.lng.toFixed(2)}`);
+});
+
+// Creates the search function
+L.Control.geocoder().addTo(map);
