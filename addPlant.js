71286b78dfd388ab822d4a00d65f482ca5936372
ashytrashbean <annalu.jansson@gmail.com>
2026-04-10 13:48:45 +0200

    added map to add plants

--- addPlant.js
+++ addPlant.js
@@ -4,9 +4,75 @@ const form = document.querySelector("#add-plant-form");
 const plantName = document.querySelector("#plant-name");
 const plantType = document.querySelector("#plant-type");
 const plantImage = document.querySelector("#plant-image");
-// const plantLocation = document.querySelector("#plant-location");
+const plantLocation = document.querySelector("#plant-location");
 const plantTime = document.querySelector("#plant-time");
 const brightnessLevel = document.querySelector("#brightnessLevel");
+
+
+let pinIcon = L.icon({
+    iconUrl: './images/pin-logo.png',
+
+    iconSize:     [38, 50], // size of the icon
+    iconAnchor:   [19, 50], // point of the icon which will correspond to marker's location
+    popupAnchor:  [0, -50] // point from which the popup should open relative to the iconAnchor
+});
+
+// inside the setView([latitude, longitude], map view distance)
+const map =L.map('map').setView([61.52, 12.74], 4);
+
+// This is purely the map object itself
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// Creates the search function
+L.Control.geocoder().addTo(map);
+
+const mockLibraries = [
+    {
+        city: "Stockholm",
+        library: "Stockholm Public Library (Asplundhuset)",
+        coordinates:[59.3434,18.0543]
+    },
+    {
+        city: "Stockholm",
+        library: "National Library of Sweden (Kungliga bibl.)",
+        coordinates:[59.3384, 18.0722]
+    },
+    {
+        city: "Uppsala",
+        library: "Uppsala Public Library",
+        coordinates:[59.8604, 17.6366]
+    },
+    {
+        city: "Uppsala",
+        library: "Carolina Rediviva (Uppsala University)",
+        coordinates:[59.8549, 17.6311]
+    },
+    {
+        city: "Västerås",
+        library: "Västerås City Library",
+        coordinates:[59.6119, 16.5448]
+    },
+]
+
+function GetMeetups(map){
+    const markers = new L.MarkerClusterGroup();
+    const markerMap= {};
+
+    mockLibraries.forEach(biblio =>{
+        const marker = L.marker(biblio.coordinates, {icon: pinIcon});
+
+        marker.bindPopup(`<b>${biblio.library}</b>`)
+        markers.addLayer(marker);
+        markerMap[biblio.library] = marker;
+    })
+    map.addLayer(markers)
+}
+
+GetMeetups(map);
+
 
 form.addEventListener("submit", async (e) => {
     e.preventDefault();
